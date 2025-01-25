d81f8faaf04c68f7729990e2b9e9abc28c0af4ae
Ryan <ryannarelli12@gmail.com>
2025-01-25 01:20:50 -0300

    enviando estilizações em mini cardsda hero

--- src/app/components/MiniCard/index.tsx
+++ src/app/components/MiniCard/index.tsx
@@ -16,16 +16,16 @@ export const MiniCard = ({ name, price, img, stars }: CustomMiniCardProps) => {
     <div className="flex justify-center mx-2">
       <div className="px-2 py-2 flex bg-white rounded-xl drop-shadow-sm overflow-hidden w-60 transform transition-transform items-center border-0 shadow-lg">
         <img
-          className="aspect-square h-16 object-cover rounded-lg"
+          className="aspect-square h-20 object-cover rounded-lg"
           src={img}
           alt="product image"
         />
-        <div className="p-2 flex flex-col justify-between">
-          <h3 className="text-sm font-bold text-gray-700 leading-4">{name}</h3>
+        <div className="p-2 flex flex-col justify-between gap-1">
+          <h3 className="text-lg font-medium text-gray-700 leading-4">{name}</h3>
           <div className="pb-0">
             <CustomRating rating={rating} />
           </div>
-          <p>R${price},00</p>
+          <p><span className="text-colorPrimary text-base">R$</span> <span className="text-xl font-semibold">{price},00</span></p>
         </div>
       </div>
     </div>
