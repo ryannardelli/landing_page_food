87609b86d59e3276c0df0fdf1e925bb780625feb
Ryan <ryannarelli12@gmail.com>
2025-01-27 00:50:30 -0300

    enviando alterações

--- src/app/components/BubbleReverse/index.tsx
+++ src/app/components/BubbleReverse/index.tsx
@@ -2,10 +2,10 @@ export const BubbleReverse = () => {
   return (
     <div className="flex items-center justify-center">
       <div className="relative bg-white p-4 rounded-3xl shadow-lg max-w-sm">
-        <p className="text-colorPrimary font-semibold text-xl">
+        <p className="text-[#202020] font-semibold text-xl">
           Nossa melhor Chef 😁
         </p>
-        <div className="absolute -top-3 transform -translate-x-1/4 w-3 h-6 bg-white rotate-180 shadow-lg"></div>
+        <div className="absolute -top-3 transform -translate-x-1/3 w-3 h-6 bg-white rotate-180 shadow-lg"></div>
       </div>
     </div>
   );
